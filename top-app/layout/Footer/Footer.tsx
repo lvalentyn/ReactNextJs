b430baeb72ddfc2a7226645e67067072337b9b25
Ewig <valikallstars@gmail.com>
2022-03-16 22:13:11 +0200

    added styles for layout footer components

--- top-app/layout/Footer/Footer.tsx
+++ top-app/layout/Footer/Footer.tsx
@@ -1,12 +1,16 @@
 import { FooterProps } from "./Footer.props";
 import styles from './Footer.module.css';
 import cn from 'classnames';
+import { format } from "date-fns";
 
-
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
 	return (
-		<div {...props}>
-			Footer
-		</div>
+		<footer className={cn(className, styles.footer)}{...props}>
+			<div className={styles.copy}>
+				OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены
+			</div>
+			<a href="#" aria-label="agrees" target="_blank" className={styles.link}>Пользовательское соглашение</a>
+			<a href="#" aria-label="confindent" target="_blank" className={styles.link}>Политика конфиденциальности</a>
+		</footer>
 	);
 };
