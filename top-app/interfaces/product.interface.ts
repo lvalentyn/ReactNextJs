4ad6010f173c0ff29ac42608a764cd0726aba22d
Ewig <valikallstars@gmail.com>
2022-03-17 18:54:08 +0200

    added interfaces components

---
+++ top-app/interfaces/product.interface.ts
@@ -0,0 +1,35 @@
+export interface ProductCharacteristic {
+	name: string;
+	value: string;
+}
+
+export interface ReviewModel {
+	_id: string;
+	name: string;
+	title: string;
+	description: string;
+	rating: number;
+	createdAt: Date;
+}
+
+export interface ProductModel {
+	_id: string;
+	categories: string[];
+	tags: string[];
+	title: string;
+	link: string;
+	price: number;
+	description: string;
+	credit: number;
+	oldPrice: number;
+	characteristics: ProductCharacteristic[];
+	createdAt: Date;
+	updatedAt: Date;
+	__v: number;
+	image: string;
+	initialRating: number;
+	reviews: ReviewModel[];
+	reviewCount: number;
+	reviewAvg?: number;
+	advantages: string;
+}
--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -1,26 +1,50 @@
+import { GetStaticProps } from "next";
 import { useState } from "react";
 import { Htag, Button, P, HashTags, Rating } from "../components";
 import { withLayout } from "../layout/Layout";
+import axios from 'axios';
+import { MenuItem } from "../interfaces/menu.interface";
 
-function Home(): JSX.Element {
-  const [rating, setRating] = useState<number>(4);
+function Home({ menu, firstCategory }: HomeProps): JSX.Element {
+	const [rating, setRating] = useState<number>(4);
 
 
-  return (
-    <>
-      <Htag tag='h1'>Title </Htag>
-      <P> Text1 </P>
-      <P fontSize="md"> Text2 </P>
-      <P fontSize="lg"> Text3 </P>
-      <HashTags size="sm"> Text3 </HashTags>
-      <HashTags size="md" color="red"> Text3 </HashTags>
-      <HashTags size="sm" color="green"> Text3 </HashTags>
-      <HashTags size="md" color="primary"> Text3 </HashTags>
-      <Button appearance='primary' arrow="right"> Click </Button>
-      <Button appearance='ghost'> Click </Button>
-      <Rating rating={rating} isEditable setRating={setRating}>  </Rating>
-    </>
-  );
+	return (
+		<>
+			<Htag tag='h1'>Title </Htag>
+			<P> Text1 </P>
+			<P fontSize="md"> Text2 </P>
+			<P fontSize="lg"> Text3 </P>
+			<HashTags size="sm"> Text3 </HashTags>
+			<HashTags size="md" color="red"> Text3 </HashTags>
+			<HashTags size="sm" color="green"> Text3 </HashTags>
+			<HashTags size="md" color="primary"> Text3 </HashTags>
+			<Button appearance='primary' arrow="right"> Click </Button>
+			<Button appearance='ghost'> Click </Button>
+			<Rating rating={rating} isEditable setRating={setRating} />
+			<ul>
+				{menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+			</ul>
+		</>
+	);
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+		firstCategory
+	});
+	return {
+		props: {
+			menu,
+			firstCategory
+		}
+	};
+};
+
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
+}
